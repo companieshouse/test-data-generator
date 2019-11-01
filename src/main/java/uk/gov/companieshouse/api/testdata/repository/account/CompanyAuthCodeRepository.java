5c75f78a768784234b0cfe5e5411f8839e9b0fa1
Rafa <rgarcia@companieshouse.gov.uk>
2019-11-01 14:21:27 +0000

    Fix imports after merge

--- src/main/java/uk/gov/companieshouse/api/testdata/repository/account/CompanyAuthCodeRepository.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/repository/account/CompanyAuthCodeRepository.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.api.testdata.repository.account;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import uk.gov.companieshouse.api.testdata.model.companyauthcode.CompanyAuthCode;
 
 import uk.gov.companieshouse.api.testdata.model.account.CompanyAuthCode;
 
--- src/test/java/uk/gov/companieshouse/api/testdata/service/TestDataServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/testdata/service/TestDataServiceImplTest.java
@@ -1,24 +1,25 @@
 package uk.gov.companieshouse.api.testdata.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import uk.gov.companieshouse.api.testdata.exception.DataException;
 import uk.gov.companieshouse.api.testdata.exception.NoDataFoundException;
 import uk.gov.companieshouse.api.testdata.model.CreatedCompany;
-import uk.gov.companieshouse.api.testdata.model.companyauthcode.CompanyAuthCode;
+import uk.gov.companieshouse.api.testdata.model.account.CompanyAuthCode;
 import uk.gov.companieshouse.api.testdata.model.companyprofile.Company;
 import uk.gov.companieshouse.api.testdata.model.filinghistory.FilingHistory;
 import uk.gov.companieshouse.api.testdata.model.officer.Officer;
 import uk.gov.companieshouse.api.testdata.model.psc.PersonsWithSignificantControl;
 import uk.gov.companieshouse.api.testdata.service.impl.TestDataServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TestDataServiceImplTest {
