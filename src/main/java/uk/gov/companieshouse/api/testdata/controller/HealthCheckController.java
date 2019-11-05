9868bdfc14da39ac9ee20238643d7a5c8161a9b1
ICoxCH <ICox@companieshouse.gov.uk>
2019-11-05 17:44:33 +0000

    Removed unused import

--- src/main/java/uk/gov/companieshouse/api/testdata/controller/HealthCheckController.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/controller/HealthCheckController.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.api.testdata.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
