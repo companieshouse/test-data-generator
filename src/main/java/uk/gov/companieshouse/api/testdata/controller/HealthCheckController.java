30dcecbb9153f7da512d5de60b53f922afd893d6
Rafa <rgarcia@companieshouse.gov.uk>
2019-11-05 16:37:53 +0000

    Use GetMapping annotation

--- src/main/java/uk/gov/companieshouse/api/testdata/controller/HealthCheckController.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/controller/HealthCheckController.java
@@ -3,8 +3,8 @@ package uk.gov.companieshouse.api.testdata.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * HealthCheck returns a 200 response if the service is running.
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<Void> isHealthy() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
