85a7ba983702590398f0908a4ebe5364a400a748
ICoxCH <ICox@companieshouse.gov.uk>
2019-11-08 09:44:17 +0000

    Check length of generated id with salt

--- src/test/java/uk/gov/companieshouse/api/testdata/service/RandomServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/testdata/service/RandomServiceImplTest.java
@@ -3,6 +3,8 @@ package uk.gov.companieshouse.api.testdata.service;
 import org.junit.jupiter.api.Test;
 import uk.gov.companieshouse.api.testdata.service.impl.RandomServiceImpl;
 
+import java.util.Base64;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -20,5 +22,8 @@ class RandomServiceImplTest {
     void getEncodedIdWithSalt() {
         String randomEncodedWithSalt = this.randomService.getEncodedIdWithSalt(5, 5);
         assertNotNull(randomEncodedWithSalt);
+
+        String randomDecodedWithSalt = new String(Base64.getUrlDecoder().decode(randomEncodedWithSalt));
+        assertEquals(10, randomDecodedWithSalt.length());
     }
 }
