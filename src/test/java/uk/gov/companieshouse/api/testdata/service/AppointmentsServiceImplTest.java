572539e9c9271330f459bd0d48877a435db92847
ICoxCH <ICox@companieshouse.gov.uk>
2019-11-13 12:57:15 +0000

    Added unit tests to match new spec

--- src/test/java/uk/gov/companieshouse/api/testdata/service/AppointmentsServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/testdata/service/AppointmentsServiceImplTest.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.api.testdata.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
@@ -20,15 +21,23 @@ import com.mongodb.MongoException;
 
 import uk.gov.companieshouse.api.testdata.exception.DataException;
 import uk.gov.companieshouse.api.testdata.exception.NoDataFoundException;
+import uk.gov.companieshouse.api.testdata.model.entity.Address;
 import uk.gov.companieshouse.api.testdata.model.entity.Appointment;
+import uk.gov.companieshouse.api.testdata.model.entity.AppointmentData;
+import uk.gov.companieshouse.api.testdata.model.entity.Links;
 import uk.gov.companieshouse.api.testdata.repository.AppointmentsRepository;
 import uk.gov.companieshouse.api.testdata.service.impl.AppointmentsServiceImpl;
+
+import java.time.LocalDate;
 
 @ExtendWith(MockitoExtension.class)
 public class AppointmentsServiceImplTest {
 
     private static final String COMPANY_NUMBER = "12345678";
     private static final String ENCODED_VALUE = "ENCODED";
+    private static final Long GENERATED_INTERNAL_ID = 123456789L;
+    private static final String ENCODED_INTERNAL_ID = "ENCODED 2";
+    private static final String ETAG = "ETAG";
 
     @Mock
     private AppointmentsRepository repository;
@@ -41,6 +50,9 @@ public class AppointmentsServiceImplTest {
     @Test
     void create() throws DataException {
         when(this.randomService.getEncodedIdWithSalt(10, 8)).thenReturn(ENCODED_VALUE);
+        when(this.randomService.getNumber(9)).thenReturn(GENERATED_INTERNAL_ID);
+        when(this.randomService.addSaltAndEncode("8" + GENERATED_INTERNAL_ID, 8)).thenReturn(ENCODED_INTERNAL_ID);
+        when(this.randomService.getEtag()).thenReturn(ETAG);
         Appointment savedApt = new Appointment();
         when(this.repository.save(any())).thenReturn(savedApt);
         
@@ -53,8 +65,45 @@ public class AppointmentsServiceImplTest {
 
         Appointment appointment = aptCaptor.getValue();
         assertNotNull(appointment);
-        assertEquals(COMPANY_NUMBER, appointment.getCompanyNumber());
         assertEquals(ENCODED_VALUE, appointment.getId());
+        assertEquals(LocalDate.now(), appointment.getCreated().getAt().toLocalDate());
+        assertEquals("8" + GENERATED_INTERNAL_ID, appointment.getInternalId());
+        assertEquals(ENCODED_VALUE, appointment.getAppointmentId());
+        assertEquals("Company " + COMPANY_NUMBER, appointment.getCompanyName());
+        assertEquals("active", appointment.getCompanyStatus());
+        assertEquals(ENCODED_INTERNAL_ID, appointment.getOfficerId());
+        assertEquals(COMPANY_NUMBER, appointment.getCompanyNumber());
+        assertEquals(LocalDate.now(), appointment.getUpdated().getAt().toLocalDate());
+
+        AppointmentData data = appointment.getData();
+
+        assertEquals("British", data.getNationality());
+        assertEquals("Director", data.getOccupation());
+        assertTrue(data.isServiceAddressIsSameAsRegisteredOfficeAddress());
+        assertEquals("Wales", data.getCountryOfResidence());
+        assertEquals(LocalDate.now(), data.getUpdatedAt().toLocalDate());
+        assertEquals("Test", data.getForename());
+        assertEquals(LocalDate.now(), data.getAppointedOn());
+        assertEquals("director", data.getOfficerRole());
+        assertEquals(ETAG, data.getEtag());
+
+        Address serviceAddress = data.getServiceAddress();
+        assertEquals("United Kingdom", serviceAddress.getCountry());
+        assertEquals("CF14 3UZ", serviceAddress.getPostalCode());
+        assertEquals("Companies House", serviceAddress.getAddressLine1());
+        assertEquals("Crownway", serviceAddress.getAddressLine2());
+        assertEquals("Cardiff", serviceAddress.getLocality());
+
+        assertEquals(COMPANY_NUMBER, data.getCompanyNumber());
+
+        Links links = data.getLinks();
+        assertEquals("/company/" + COMPANY_NUMBER + "/appointments/" + ENCODED_INTERNAL_ID, links.getSelf());
+        Links officerLink = links.getOfficer();
+        assertEquals("/officers/" + ENCODED_INTERNAL_ID, officerLink.getSelf());
+        assertEquals("/officers/" + ENCODED_INTERNAL_ID + "/appointments", officerLink.getAppointments());
+
+        assertEquals("DIRECTOR", data.getSurname());
+        assertEquals(LocalDate.now(), data.getDateOfBirth());
     }
 
     @Test
