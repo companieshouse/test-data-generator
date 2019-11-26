bb8fa21144a9e38cc33ce204f1340bd44acc28af
ICoxCH <ICox@companieshouse.gov.uk>
2019-11-26 10:57:11 +0000

    Removed redundant test case

--- src/test/java/uk/gov/companieshouse/api/testdata/service/impl/OfficerAppointmentServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/testdata/service/impl/OfficerAppointmentServiceImplTest.java
@@ -49,7 +49,7 @@ class OfficerAppointmentServiceImplTest {
     private OfficerAppointmentServiceImpl officerListService;
 
     @Test
-    void createEnglandWales() throws DataException {
+    void create() throws DataException {
         Address mockAddress = new Address();
         CompanySpec spec = new CompanySpec();
         spec.setCompanyNumber(COMPANY_NUMBER);
@@ -83,50 +83,11 @@ class OfficerAppointmentServiceImplTest {
         assertEquals(3, officerAppointment.getDateOfBirthMonth().intValue());
         assertEquals("Test DIRECTOR", officerAppointment.getName());
 
-        assertOfficerItem(officerAppointment.getOfficerAppointmentItems().get(0), "Wales");
+        assertOfficerItem(officerAppointment.getOfficerAppointmentItems().get(0));
 
     }
 
-    @Test
-    void createScotland() throws DataException {
-        Address mockAddress = new Address();
-        CompanySpec spec = new CompanySpec();
-        spec.setCompanyNumber(COMPANY_NUMBER);
-        spec.setJurisdiction(Jurisdiction.ENGLAND_WALES);
-
-        when(randomService.getEtag()).thenReturn(ETAG);
-        OfficerAppointment savedOfficerAppointment = new OfficerAppointment();
-        when(addressService.getCountryOfResidence(spec.getJurisdiction())).thenReturn("Scotland");
-        when(addressService.getAddress(spec.getJurisdiction())).thenReturn(mockAddress);
-        when(repository.save(Mockito.any())).thenReturn(savedOfficerAppointment);
-
-        OfficerAppointment returnedOfficerAppointment = this.officerListService.create(spec, OFFICER_ID, APPOINTMENT_ID);
-
-        assertEquals(savedOfficerAppointment, returnedOfficerAppointment);
-
-        ArgumentCaptor<OfficerAppointment> officerCaptor = ArgumentCaptor.forClass(OfficerAppointment.class);
-        verify(repository).save(officerCaptor.capture());
-        OfficerAppointment officerAppointment = officerCaptor.getValue();
-
-        assertEquals(OFFICER_ID, officerAppointment.getId());
-        assertNotNull(officerAppointment.getCreatedAt());
-        assertNotNull(officerAppointment.getUpdatedAt());
-        assertEquals(1, officerAppointment.getTotalResults().intValue());
-        assertEquals(1, officerAppointment.getActiveCount().intValue());
-        assertEquals(0, officerAppointment.getInactiveCount().intValue());
-        assertEquals(1, officerAppointment.getResignedCount().intValue());
-        assertFalse(officerAppointment.getCorporateOfficer());
-        assertEquals("/officers/" + OFFICER_ID + "/appointments", officerAppointment.getLinks().getSelf());
-        assertEquals(ETAG, officerAppointment.getEtag());
-        assertEquals(1990, officerAppointment.getDateOfBirthYear().intValue());
-        assertEquals(3, officerAppointment.getDateOfBirthMonth().intValue());
-        assertEquals("Test DIRECTOR", officerAppointment.getName());
-
-        assertOfficerItem(officerAppointment.getOfficerAppointmentItems().get(0), "Scotland");
-
-    }
-
-    private void assertOfficerItem(OfficerAppointmentItem item, String companyOfResidence) {
+    private void assertOfficerItem(OfficerAppointmentItem item) {
 
         assertEquals("Director", item.getOccupation());
         assertNotNull(item.getAddress());
@@ -136,7 +97,7 @@ class OfficerAppointmentServiceImplTest {
         assertEquals("/company/" + COMPANY_NUMBER + "/appointments/" + APPOINTMENT_ID,
                 item.getLinks().getSelf());
         assertEquals("/company/" + COMPANY_NUMBER, item.getLinks().getCompany());
-        assertEquals(companyOfResidence, item.getCountryOfResidence());
+        assertEquals("Wales", item.getCountryOfResidence());
         assertNotNull(item.getAppointedOn());
         assertEquals("British", item.getNationality());
         assertNotNull(item.getUpdatedAt());
