cbfe25e773644c1dcb13ef399b2b51665ac87752
Lakshmi Yadavalli <lyadavalli@companieshouse.gov.uk>
2024-12-09 17:50:10 +0000

    removed public class modifier for CompanySpecTest

--- src/test/java/uk/gov/companieshouse/api/testdata/spec/CompanySpecTest.java
+++ src/test/java/uk/gov/companieshouse/api/testdata/spec/CompanySpecTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
-public class CompanySpecTest {
+class CompanySpecTest {
 
     private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     private Validator validator;
