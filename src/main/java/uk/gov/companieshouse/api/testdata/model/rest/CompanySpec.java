a34e0ccb0363087b86161632d8c849e00f703e28
Lakshmi Yadavalli <lyadavalli@companieshouse.gov.uk>
2025-01-27 12:06:21 +0000

    renamed subtype json property

--- src/main/java/uk/gov/companieshouse/api/testdata/model/rest/CompanySpec.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/model/rest/CompanySpec.java
@@ -26,7 +26,7 @@ public class CompanySpec {
     @Pattern(regexp = "assurance-company|charitable-incorporated-organisation|company_delta|converted-or-closed|eeig|eeig-establishment|european-public-limited-liability-company-se|fake-type|further-education-or-sixth-form-college-corporation|icvc-securities|icvc-umbrella|icvc-warrant|industrial-and-provident-society|invalid|investment-company-with-variable-capital|limited-partnership|llp|ltd|northern-ireland|northern-ireland-other|old-public-company|other|oversea-company|plc|private-limited-guarant-nsc|private-limited-guarant-nsc-limited-exemption|private-limited-shares-section-30-exemption|private-unlimited|private-unlimited-nsc|protected-cell-company|registered-overseas-entity|registered-society-non-jurisdictional|royal-charter|scottish-charitable-incorporated-organisation|scottish-partnership|uk-establishment|ukeig|united-kingdom-societas|unregistered-company", message = "Invalid company type")
     private String companyType;
 
-    @JsonProperty("subtype")
+    @JsonProperty("sub_type")
     @Pattern(regexp = "community-interest-company|private-fund-limited-partnership",
             message = "Invalid company subtype")
     private String subType;
