71a1a0ab4e64204c2c2ddc58f55dd499dfa2fb3e
gzcomphouse <gzealley@companieshouse.gov.uk>
2025-03-10 15:01:37 +0000

    Set partial-data-available on created company (#118)

--- src/main/java/uk/gov/companieshouse/api/testdata/model/entity/CompanyProfile.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/model/entity/CompanyProfile.java
@@ -172,6 +172,8 @@ public class CompanyProfile {
     private Boolean isCommunityInterestCompany;
     @Field ("data.company_status_detail")
     private String companyStatusDetail;
+    @Field("data.partial_data_available")
+    private String partialDataAvailable;
 
     public String getId() {
         return id;
@@ -340,5 +342,11 @@ public class CompanyProfile {
 
     public void setCompanyStatusDetail(String companyStatusDetail) {
         this.companyStatusDetail = companyStatusDetail;
+    }
+
+    public String getPartialDataAvailable() {return this.partialDataAvailable; }
+
+    public void setPartialDataAvailable(String partialDataAvailable) {
+        this.partialDataAvailable = partialDataAvailable;
     }
 }
