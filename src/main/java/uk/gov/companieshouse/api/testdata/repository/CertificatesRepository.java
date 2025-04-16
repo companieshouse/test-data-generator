c992ec4ff1017bce09844be3c4d01beeb8f04a4e
bsrinivasancompanieshouse <bsrinivasan@companieshouse.gov.uk>
2025-04-16 09:11:30 +0100

    fixed sonarqube issue

--- src/main/java/uk/gov/companieshouse/api/testdata/repository/CertificatesRepository.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/repository/CertificatesRepository.java
@@ -1,9 +1,13 @@
 package uk.gov.companieshouse.api.testdata.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import uk.gov.companieshouse.api.testdata.model.entity.Certificates;
 
 @NoRepositoryBean
 public interface CertificatesRepository extends MongoRepository<Certificates, String> {
+
+    Optional<Certificates> findById(String certificatesId);
 }
