09a621436aac31ad4e2cc005a1cff839efbf13df
bsrinivasancompanieshouse <bsrinivasan@companieshouse.gov.uk>
2025-04-17 16:14:47 +0100

    removed unnecessary import

--- src/main/java/uk/gov/companieshouse/api/testdata/repository/CertificatesRepository.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/repository/CertificatesRepository.java
@@ -1,7 +1,5 @@
 package uk.gov.companieshouse.api.testdata.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import uk.gov.companieshouse.api.testdata.model.entity.Certificates;
