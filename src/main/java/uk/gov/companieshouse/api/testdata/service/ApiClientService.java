cc1c60270e4280941288319c8e53f749cd0f576e
lyadavalli <lyadavalli@companieshouse.gov.uk>
2026-01-14 15:03:05 +0000

    code refactoring for internal api client

---
+++ src/main/java/uk/gov/companieshouse/api/testdata/service/ApiClientService.java
@@ -0,0 +1,24 @@
+package uk.gov.companieshouse.api.testdata.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
+
+@Service
+public class ApiClientService {
+
+    private final String privateAccountUrl;
+
+    public ApiClientService(@Value("${account.api.url}") String privateAccountUrl) {
+        this.privateAccountUrl = privateAccountUrl;
+    }
+
+    public InternalApiClient getInternalApiClientForPrivateAccountApiUrl() {
+        final var internalApiClient = ApiSdkManager.getInternalSDK();
+        internalApiClient.setInternalBasePath(privateAccountUrl);
+        return internalApiClient;
+    }
+}
