f961798da0bf03c17bc38cfb88db3dad91e9ac17
kuzonduch <kuzondu@companieshouse.gov.uk>
2026-01-29 15:37:24 +0000

    adding right tags to repository bean

--- src/main/java/uk/gov/companieshouse/api/testdata/repository/UserCompanyAssociationRepository.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/repository/UserCompanyAssociationRepository.java
@@ -1,10 +1,11 @@
 package uk.gov.companieshouse.api.testdata.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 import uk.gov.companieshouse.api.testdata.model.entity.UserCompanyAssociation;
 
-@Repository
+@NoRepositoryBean
 public interface UserCompanyAssociationRepository extends MongoRepository<UserCompanyAssociation, String> {
 
 }
