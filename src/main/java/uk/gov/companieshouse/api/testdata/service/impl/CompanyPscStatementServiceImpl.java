8eaff20819da6cfd3ae6273e69c41b58ed5f6dcb
Ade Harrison <109078207+adeharrison-ch@users.noreply.github.com>
2026-02-17 15:00:48 +0000

    Revert "asm 1420 fix test data generator vulnerabilities"

--- src/main/java/uk/gov/companieshouse/api/testdata/service/impl/CompanyPscStatementServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/service/impl/CompanyPscStatementServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
