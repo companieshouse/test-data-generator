be7392870f904d27bd3603bc6dc269d175bbc2f7
bsrinivasancompanieshouse <bsrinivasan@companieshouse.gov.uk>
2026-06-09 10:47:14 +0100

    added transaction tag for deletebyEmail

--- src/main/java/uk/gov/companieshouse/api/testdata/service/impl/UserServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/service/impl/UserServiceImpl.java
@@ -250,6 +250,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public boolean deleteByEmail(String email) {
         LOG.info("delete called for email={}");
 
