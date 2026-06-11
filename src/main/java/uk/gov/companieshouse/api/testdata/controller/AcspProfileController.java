12cffd4501575a7063181bfd7aadec4700680ca1
lyadavalli <lyadavalli@companieshouse.gov.uk>
2026-06-11 11:19:38 +0100

    refactoring acsp structure

--- src/main/java/uk/gov/companieshouse/api/testdata/controller/AcspProfileController.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/controller/AcspProfileController.java
@@ -8,12 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.companieshouse.api.testdata.Application;
 import uk.gov.companieshouse.api.testdata.exception.NoDataFoundException;
 import uk.gov.companieshouse.api.testdata.model.entity.AcspProfile;
 import uk.gov.companieshouse.api.testdata.service.AcspWorkflowService;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 
 @RestController
 @RequestMapping(value = "${api.endpoint}/internal", produces = MediaType.APPLICATION_JSON_VALUE)
