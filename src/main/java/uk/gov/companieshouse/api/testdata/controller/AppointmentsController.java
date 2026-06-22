70ec422fc422ba5a732051d1f33e25a760d613be
bsrinivasancompanieshouse <bsrinivasan@companieshouse.gov.uk>
2026-06-22 12:06:29 +0100

    Appointments controller initial commit

---
+++ src/main/java/uk/gov/companieshouse/api/testdata/controller/AppointmentsController.java
@@ -0,0 +1,53 @@
+package uk.gov.companieshouse.api.testdata.controller;
+
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.companieshouse.api.testdata.Application;
+import uk.gov.companieshouse.api.testdata.exception.DataException;
+import uk.gov.companieshouse.api.testdata.model.rest.request.AppointmentCreationRequest;
+import uk.gov.companieshouse.api.testdata.model.rest.response.AppointmentsResultResponse;
+import uk.gov.companieshouse.api.testdata.service.AppointmentService;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+@RestController
+@RequestMapping(value = "${api.endpoint}/internal", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AppointmentsController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.APPLICATION_NAME);
+    private static final String STATUS = "status";
+
+    private final AppointmentService appointmentService;
+
+    public AppointmentsController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping("/appointments")
+    public ResponseEntity<AppointmentsResultResponse> createAppointments(
+            @Valid @RequestBody AppointmentCreationRequest request) throws DataException {
+
+        if (request.getCompanyNumber() == null) {
+            throw new DataException("Company Number is required to create appointments");
+        }
+
+        try {
+            var createdAppointments = appointmentService.createAppointment(request);
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("Appointment-id", createdAppointments.getAppointment().getFirst().getId());
+            LOG.info("New appointments added", data);
+            return new ResponseEntity<>(createdAppointments, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            throw new DataException("Error creating appointments", ex);
+        }
+    }
+}
