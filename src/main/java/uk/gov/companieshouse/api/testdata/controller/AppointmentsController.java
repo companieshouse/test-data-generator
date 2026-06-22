216d6bc5816d38826d29d3be29a4f2f2f57b15e0
bsrinivasancompanieshouse <bsrinivasan@companieshouse.gov.uk>
2026-06-22 12:06:29 +0100

    Appointments controller initial commit
    # Conflicts:
    #	src/main/java/uk/gov/companieshouse/api/testdata/controller/AppointmentsController.java

--- src/main/java/uk/gov/companieshouse/api/testdata/controller/AppointmentsController.java
+++ src/main/java/uk/gov/companieshouse/api/testdata/controller/AppointmentsController.java
@@ -1,10 +1,20 @@
 package uk.gov.companieshouse.api.testdata.controller;
 
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.testdata.Application;
+import uk.gov.companieshouse.api.testdata.exception.DataException;
+import uk.gov.companieshouse.api.testdata.model.rest.request.AppointmentCreationRequest;
+import uk.gov.companieshouse.api.testdata.model.rest.response.AppointmentsResultResponse;
 import uk.gov.companieshouse.api.testdata.service.AppointmentService;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
@@ -23,6 +33,26 @@ public class AppointmentsController {
 
     public AppointmentsController(AppointmentService appointmentService) {
         this.appointmentService = appointmentService;
+    }
+
+    @PostMapping("/appointments")
+    public ResponseEntity<AppointmentsResultResponse> createAppointments(
+            @Valid @RequestBody AppointmentCreationRequest request) throws DataException {
+
+        if (request.getCompanyNumber() == null) {
+            throw new DataException("Company Number is required to create appointments");
+        }
+
+        try {
+            var createdAppointments = appointmentService.createAppointment(request);
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("Appointment-id", createdAppointments.getAppointment().getFirst().getId());
+            LOG.info("New appointments added", data);
+            return new ResponseEntity<>(createdAppointments, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            throw new DataException("Error creating appointments", ex);
+        }
     }
 
     @DeleteMapping("/company/{companyNumber}/appointments")
